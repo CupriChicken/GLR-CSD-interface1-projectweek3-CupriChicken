90b25ec447cb8e8621a8dd8b32a9bfb07a21816e
CupriChicken <101941@glr.nl>
2025-04-09 13:26:21 +0000

    Afgerond

--- javascript/hamburger.js
+++ javascript/hamburger.js
@@ -1,3 +1,14 @@
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+
 document.getElementById("hamburger").addEventListener("click", function() {
     document.getElementById("mobile-menu").classList.toggle("visible");
 });
